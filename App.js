9da8cd69388f50a58faeac1ead76fad932208354
Dragusin Cristian <dragusincristian609@gmail.com>
2023-08-21 22:42:01 +0300

    added logout logic

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { useContext } from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
@@ -7,7 +9,7 @@ import SignupScreen from './screens/SignupScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import { Colors } from './constants/styles';
 import AuthContextProvider, { AuthContext } from './store/auth-context';
-import { useContext } from 'react';
+import IconButton from './components/ui/IconButton'
 
 //* the docs for firebase auth rest sdk: https://firebase.google.com/docs/reference/rest/auth
 
@@ -29,6 +31,8 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const authCtx = useContext(AuthContext)
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -37,7 +41,19 @@ function AuthenticatedStack() {
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon={'exit'}
+              color={tintColor}
+              size={24}
+              onPress={authCtx.logout}
+            />)
+        }}
+      />
     </Stack.Navigator>
   );
 }
