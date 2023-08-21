57cd14298dfc14bc64fd8e5bbce470ca118ee03f
Dragusin Cristian <dragusincristian609@gmail.com>
2023-08-21 22:55:42 +0300

    accessing protected BE resources for auth users

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,26 @@
+import { useContext, useEffect, useState } from 'react';
+import axios, { AxiosError } from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const authCtx = useContext(AuthContext)
+  const { token } = authCtx
+  const [fetchedMessage, setFetchedMessage] = useState('')
+
+  useEffect(() => {
+    axios.get(
+      'https://rn-expenses-tracker-38091-default-rtdb.asia-southeast1.firebasedatabase.app/message.json?auth=' + token
+    ).then(response => {
+      setFetchedMessage(response.data)
+    })
+  }, [token])
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
