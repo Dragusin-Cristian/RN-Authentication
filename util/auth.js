adac5180f78d229d36e385098527d90004691239
Dragusin Cristian <dragusincristian609@gmail.com>
2023-08-21 22:11:59 +0300

    added signup and login logic

---
+++ util/auth.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { FIREBASE_API_KEY } from '@env'
+
+export const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${FIREBASE_API_KEY}`
+  const response = await axios.post(
+    url,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    });
+
+    console.log(response);
+}
+
+export const createUser = async (email, password) => {
+  await authenticate('signUp', email, password)
+}
+
+export const login = async (email, password) => {
+  await authenticate('signInWithPassword', email, password)
+}
+
+
